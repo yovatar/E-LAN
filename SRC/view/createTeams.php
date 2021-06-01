844657d5c37d78657d97a389b1bb074a80c88bbb
BONZON Yoann <Yoann.BONZON@cpnv.ch>
2021-06-01 08:12:17 +0200

    image createTeams

--- SRC/view/createTeams.php
+++ SRC/view/createTeams.php
@@ -22,6 +22,9 @@ function viewCreateTeams()
             <input type="shortname" id="shortname" name="shortname" placeholder="LFK" required
                    class="border-2 px-4 py-2 rounded-md border-blueGray-200 focus:outline-none focus:ring-2 focus:ring-purple-500">
             <div class="flex flex-col space-y-2 p-px lg:flex-row lg:items-center lg:space-x-3 lg:space-y-0">
+                <label for="date">Image</label>
+                <input type="file" id="date" name="date" placeholder="31.05.2021" required
+                       class="border-2 px-4 py-2 rounded-md border-blueGray-200 focus:outline-none focus:ring-2 focus:ring-purple-500">
                 <button type="submit"
                         class="flex flex-row items-center justify-center px-4 py-2 space-x-2 text-white bg-purple-500 rounded-md hover:bg-purple-700">
                     <p>Créer l'équipe</p>
