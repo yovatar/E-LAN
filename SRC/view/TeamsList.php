1fda1e9587efd28d1741d881c99497a156e8c03e
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2021-06-15 12:24:05 +0200

    cleaned a little

--- SRC/view/TeamsList.php
+++ SRC/view/TeamsList.php
@@ -19,26 +19,28 @@ function viewTeamsList($teams, $page, $maxPage, $canCreate = false)
         </div>
     <?php } ?>
     <?php /* Search */ ?>
-    <form x-data="search" x-init="url = '/api/teams/search'" @submit="$event.preventDefault()" action="/team/search" method="POST" class="flex flex-col w-full px-2 my-2">
-        <label for="query">Rechercher</label>
-        <div class="relative w-min">
-            <input @input.debounce="post([{name:'query',value:$el.value}])" type="text" id="query" name="query" placeholder="..." class="border-2 rounded-md border-blueGray-200 focus:outline-none focus:ring-2 focus:ring-purple-500">
-            <button type="submit" class="absolute inset-y-0 right-0 flex flex-col items-center justify-center px-2 focus:outline-none focus:border-none hover:text-purple-500 focus:text-purple-500">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-            </button>
-        </div>
-        <div class="relative">
-            <div class="absolute z-10 px-3 py-1 bg-white">
-                <template x-for="result in results">
-                    <a :href="`/teams/${result.name}`">
-                        <p x-text="result.name"></p>
-                    </a>
-                </template>
+    <div class="flex justify-center">
+        <form x-data="search" @click.outside="results = []" x-init="url = '/api/teams/search'" @submit="$event.preventDefault()" action="/team/search" method="POST" class="flex flex-col w-full max-w-lg px-2 my-2">
+            <label for="query">Rechercher</label>
+            <div class="relative">
+                <input @input.debounce="post([{name:'query',value:$el.value}])" type="text" id="query" name="query" placeholder="..." autocomplete="off" class="w-full border-2 rounded-md border-blueGray-200 focus:outline-none focus:ring-2 focus:ring-purple-500">
+                <button type="submit" class="absolute inset-y-0 right-0 flex flex-col items-center justify-center px-2 focus:outline-none focus:border-none hover:text-purple-500 focus:text-purple-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </button>
             </div>
-        </div>
-    </form>
+            <div class="relative w-full" :class="results.length == 0 ? 'hidden': ''">
+                <div class="absolute z-10 flex flex-col w-full px-3 py-1 bg-white rounded-b-md filter drop-shadow-md">
+                    <template x-for="result in results">
+                        <a :href="`/teams/${result.name}`">
+                            <p x-text="result.name"></p>
+                        </a>
+                    </template>
+                </div>
+            </div>
+        </form>
+    </div>
     <?php /* List */ ?>
     <div class="flex flex-row justify-center w-full px-6 py-3 bg-white rounded-md filter drop-shadow-md">
         <div class="flex flex-col w-full space-y-3">
