a9dec53e2fe98d4704c693346e6c50e9518aa3f7
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2021-06-19 17:56:08 +0200

    added start and duration indicator in lan view

--- SRC/view/lan.php
+++ SRC/view/lan.php
@@ -44,10 +44,28 @@ function viewLan($lan, $isModerator)
 
             <div class="flex flex-col w-full space-y-2">
                 <?php foreach ($lan["events"] as $event) { ?>
-                    <div class="flex flex-col object-cover w-full h-64 px-4 py-2 overflow-hidden bg-purple-500 rounded-md md:h-48 bg-hero-endless-clouds-purple400-100" style="<?= !empty($event["path"]) ? "background:url(" . $event["path"] . ") no-repeat center center; -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;" : '' ?>">
-                        <h1 class="text-2xl font-medium text-white md:text-3xl"><?= $event["name"] ?></h1>
-                        <p class="text-lg text-white whitespace-pre-line md:text-xl"><?= $event["description"] ?></p>
-                    </div>
+                    <?php if (!empty($event["id"])) {
+                        $start = new DateTime($event["start"]);
+                        $end = new DateTime($event["end"]);
+                        $diff = $end->diff($start);
+                    ?>
+                        <div class="flex flex-col object-cover w-full h-64 px-4 py-2 overflow-hidden bg-purple-500 rounded-md md:h-48 bg-hero-endless-clouds-purple400-100" style="<?= !empty($event["path"]) ? "background:url(" . $event["path"] . ") no-repeat center center; -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;" : '' ?>">
+                            <h1 class="text-2xl font-medium text-white md:text-3xl"><?= $event["name"] ?></h1>
+                            <div class="flex flex-row items-center space-x-2 text-xl text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+                                </svg>
+                                <p><?= $start->format("Y-m-d H:i") ?></p>
+                            </div>
+                            <div class="flex flex-row items-center space-x-2 text-xl text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
+                                </svg> </svg>
+                                <p><?= $diff->d > 0 ? $diff->d . "j " : '' ?><?= str_pad($diff->h, 2, "0", STR_PAD_LEFT) ?>:<?= str_pad($diff->i, 2, "0", STR_PAD_LEFT) ?></p>
+                            </div>
+                            <p class="text-lg text-white whitespace-pre-line md:text-xl"><?= $event["description"] ?></p>
+                        </div>
+                    <?php } ?>
                 <?php } ?>
                 <?php if ($isModerator) { ?>
                     <a href="/event/create?lan=<?= $lan["name"] ?>" class="w-full px-4 py-2 font-medium text-center text-white bg-purple-500 rounded-md focus:outline-none hover:bg-purple-700 focus:bg-purple-700 focus:ring-2 focus:ring-purple-500 filter focus:drop-shadow-md">Nouvel événement</a>
